46021d6f2e7098a1742c714edd5e08220b70bc21
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-17 09:30:17 +0200

    functions split-up, buy and tripple working

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
+//prepare variable for label so we can work with it
 let label = document.getElementById("counter-label");
 //display text for start
-label.innerText = ('click this dog to give it some love.')
+label.innerText = ('This dog is in need of some attention:')
+//prepare variable for comment so we can work with it
+let comment = document.getElementById("comment");
+//display text for start
+comment.innerText = ('Let\'s give it some love by clicking on it');
+//prepare variable for run so we can work with it
+let run = document.getElementById("run");
 //start score = 0
-let score = 0;
-console.log(score);
+let points = 0;
 //each click will add 1
 let click = 1;
-console.log(click);
 
 //make sure the score adds 1 to the score every time someone clicks on the pic.
-document.getElementById("run").addEventListener("click", function getScore() {
-    let newScore = score + click++;
-    console.log(newScore);
-//display the score inside the label
-    label.innerText = (`You have shown this dog ${newScore} times love.  Give it some more!`);
+run.addEventListener("click", function getPoints() {
+    points = points + click;
+    //display the score inside the label
+    label.innerText = (`Your points: ${points}`);
+    //display a comment
+    comment.innerText = ('Keep making this dog happy and earn more!')
 });
-//add a button that will multiply each click with 3
-document.getElementById("feed").addEventListener("click", () => {
-    click += 2;
-    let newScore = score + click++;
-    console.log(newScore);
-    label.innerText = (`You have shown this dog ${newScore} times love.  Give it some more!`);
+//add a button that will allow to buy a cookie
+document.getElementById("cookie").addEventListener("click", function cookie() {
+    //set value for cookie
+    let cookie = 15;
+    if (points < 15) {
+        console.log(points);
+        //comment.innerText = ('Your score isn\'t high enough, keep giving it all your love');
+    } else {
+        //comment.innerText = ('Congratulations you just bought a cookie for the dog!');
+        points = points - cookie;
+        console.log(points)
+        label.innerText = (`Your points: ${points}`);
+        console.log(label.innerText);
+        click *= 3;
+        comment.innerText = ('That\'s right, from here on out you are getting double the points!');
+    }
 });
+
+
+
+
+
+
+
+
