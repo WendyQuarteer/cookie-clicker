46d62c92017c7d13d716f11766a59885ad88dc15
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-17 14:20:11 +0200

    change in bostonPower function & pointsvalue + buy can only be done one time

--- script.js
+++ script.js
@@ -13,11 +13,11 @@ let run = document.getElementById("run");
 //start score = 0
 let points = 0;
 //each click will add 1
-let click = 1;
+let clickPower = 1;
 
 //make sure the score adds 1 to the score every time someone clicks on the pic.
 run.addEventListener("click", function getPoints() {
-    points = points + click;
+    points = points + clickPower;
     //display the score inside the label
     label.innerText = (`Your points: ${points}`);
     //display a comment
@@ -27,24 +27,24 @@ run.addEventListener("click", function getPoints() {
 //add functions that will allow to buy and each time it will make the click-power go up:
 document.getElementById("cookie").addEventListener("click", function cookie() {
     buyMultiplier(15, 3, 'You just bought a cookie for the dog, from here on out you are getting triple the points!');
-    bostonPower(3);
-});
+    bostonPower();
+}, {once : true});
 document.getElementById("food").addEventListener("click", function food() {
     buyMultiplier(30, 5, 'You just bought food for the dog, from here on out you are getting five times the points!');
-    bostonPower(5);
-});
+    bostonPower();
+}, {once : true});
 document.getElementById("drink").addEventListener("click", function drink() {
     buyMultiplier(60, 8, 'You just bought water for the dog, from here on out you are getting eight times the points!');
-    bostonPower(8);
-});
+    bostonPower();
+}, {once : true});
 document.getElementById("walk").addEventListener("click", function walk() {
     buyMultiplier(200, 12, 'You just went for a walk with the dog, from here on out you are getting twelve times the points!');
-    bostonPower(12);
-});
+    bostonPower();
+}, {once : true});
 document.getElementById("bone").addEventListener("click", function bone() {
     buyMultiplier(500, 20, 'You just bought a bone for the dog, from here on out you are getting twenty times the points!');
-    bostonPower(20);
-});
+    bostonPower();
+}, {once : true});
 
 function buyMultiplier(price, multiplier, commentText) {
     if (points < price) {
@@ -52,12 +52,12 @@ function buyMultiplier(price, multiplier, commentText) {
     } else {
         points = points - price;
         label.innerText = (`Your points: ${points}`);
-        click *= multiplier;
+        clickPower = multiplier;
         comment.innerText = commentText;
     }
 }
-function bostonPower(multiplier) {
-    power.innerText = (`You now get ${multiplier} x the points!`);
+function bostonPower() {
+    power.innerText = (`You now get ${clickPower} x the points!`);
 }
 
 
