807f48d3c305381bc6b514ee2bf7f6878b4a7137
Wendy Quarteer <wendyquarteer@outlook.com>
2022-05-18 09:06:25 +0200

    background + table added

--- script.js
+++ script.js
@@ -14,8 +14,10 @@ let run = document.getElementById("run");
 let points = 0;
 //each click will add 1
 let clickPower = 1;
+//prepare variable for autoClick function
+let autoClickerAmount = 0;
 
-//make sure the score adds 1 to the score every time someone clicks on the pic.
+//for starters make sure the score adds 1 to the score every time someone clicks on the pic.
 run.addEventListener("click", function getPoints() {
     points = points + clickPower;
     //display the score inside the label
@@ -26,26 +28,41 @@ run.addEventListener("click", function getPoints() {
 
 //add functions that will allow to buy and each time it will make the click-power go up:
 document.getElementById("cookie").addEventListener("click", function cookie() {
-    buyMultiplier(15, 3, 'You just bought a cookie for the dog, from here on out you are getting triple the points!');
+    buyMultiplier(25, 3, 'You just bought a cookie for the dog, from here on out you are getting triple the points!');
     bostonPower();
-}, {once : true});
+}, {once: true});
 document.getElementById("food").addEventListener("click", function food() {
-    buyMultiplier(30, 5, 'You just bought food for the dog, from here on out you are getting five times the points!');
+    buyMultiplier(85, 5, 'You just bought food for the dog, from here on out you are getting five times the points!');
     bostonPower();
-}, {once : true});
+}, {once: true});
 document.getElementById("drink").addEventListener("click", function drink() {
-    buyMultiplier(60, 8, 'You just bought water for the dog, from here on out you are getting eight times the points!');
+    buyMultiplier(350, 8, 'You just bought water for the dog, from here on out you are getting eight times the points!');
     bostonPower();
-}, {once : true});
+}, {once: true});
 document.getElementById("walk").addEventListener("click", function walk() {
-    buyMultiplier(200, 12, 'You just went for a walk with the dog, from here on out you are getting twelve times the points!');
+    buyMultiplier(700, 12, 'You just went for a walk with the dog, from here on out you are getting twelve times the points!');
     bostonPower();
-}, {once : true});
+}, {once: true});
 document.getElementById("bone").addEventListener("click", function bone() {
-    buyMultiplier(500, 20, 'You just bought a bone for the dog, from here on out you are getting twenty times the points!');
+    buyMultiplier(1500, 20, 'You just bought a bone for the dog, from here on out you are getting twenty times the points!');
     bostonPower();
-}, {once : true});
+}, {once: true});
 
+//auto-clicker
+document.getElementById("auto-click").addEventListener("click", function () {
+    if (points < 1000) {
+        comment.innerText = ('Your score isn\'t high enough, keep giving it all your love');
+    } else {
+        autoClickerAmount ++;
+        power.innerText = ('Every 2 seconds you will get an automatic click!');
+    }
+});
+setInterval(() => {
+  points += autoClickerAmount;
+    label.innerText = (`Your points: ${points}`);
+}, 5000);
+
+//multiplier function
 function buyMultiplier(price, multiplier, commentText) {
     if (points < price) {
         comment.innerText = ('Your score isn\'t high enough, keep giving it all your love');
@@ -56,6 +73,7 @@ function buyMultiplier(price, multiplier, commentText) {
         comment.innerText = commentText;
     }
 }
+//function to display the power
 function bostonPower() {
     power.innerText = (`You now get ${clickPower} x the points!`);
 }
